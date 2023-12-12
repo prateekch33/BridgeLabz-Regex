0b0c682d760b5c66c0e5a06c5d82aece59072e11
prateekch33 <prateekchandra27@outlook.com>
2023-12-12 23:31:32 +0530

    Junit Bug Updated

--- src/test/java/JunitExample.java
+++ src/test/java/JunitExample.java
@@ -1,37 +1,45 @@
 import org.junit.jupiter.api.Test;
 
 import com.example.Main;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
     void testValidFirstName() {
-        assertTrue(Main.isValidFirstName("John")); // Happy case
-        assertFalse(Main.isValidFirstName("john1")); // Sad case
+        assertTrue(Main.isValidFirstName("John"));
+        assertFalse(Main.isValidFirstName("john1"));
     }
 
     @Test
     void testValidLastName() {
-        assertTrue(Main.isValidFirstName("Doe")); // Happy case
-        assertFalse(Main.isValidFirstName("Doe1")); // Sad case
+        assertTrue(Main.isValidFirstName("Doe"));
+        assertFalse(Main.isValidFirstName("Doe1"));
     }
 
     @Test
     void testValidEmail() {
-        assertTrue(Main.isValidEmail("example@example.com")); // Happy case
-        assertFalse(Main.isValidEmail("example")); // Sad case
+        assertTrue(Main.isValidEmail("example@example.com"));
+        assertFalse(Main.isValidEmail("example"));
     }
 
     @Test
     void testValidMobileNumber() {
-        assertTrue(Main.isValidMobileNumber("91 9919819801")); // Happy case
-        assertFalse(Main.isValidMobileNumber("919919819801")); // Sad case
+        assertTrue(Main.isValidMobileNumber("91 9919819801"));
+        assertFalse(Main.isValidMobileNumber("919919819801"));
     }
 
     @Test
     void testValidPassword() {
-        assertTrue(Main.isValidPassword("Password7834791@")); // Happy case
-        assertFalse(Main.isValidPassword("password")); // Sad case
+        assertTrue(Main.isValidPassword("Password@167"));
+        assertFalse(Main.isValidPassword("password")); 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"example1@example.com", "example2@example.com", "example3@example.com"})
+    void testValidEmail(String email) {
+        assertTrue(Main.isValidEmail(email));
     }
 }
